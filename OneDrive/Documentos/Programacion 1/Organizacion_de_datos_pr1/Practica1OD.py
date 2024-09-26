e3f08eea30137b1c7a7caf2ee3580ddb4ef561dd
LUIS ALEJANDRO BLANCO <157557586+AlejandroBlanco13@users.noreply.github.com>
2024-09-25 23:54:32 -0600

    Terminado

---
+++ OneDrive/Documentos/Programacion 1/Organizacion_de_datos_pr1/Practica1OD.py
@@ -0,0 +1,10 @@
+#Practica #1
+numero1 = input("Ingresa el primer número: ")
+numero2 = input("Ingresa el segundo número: ")
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+resultado = numero1 + numero2
+
+print("La suma de los dos números es:", resultado)
